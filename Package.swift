8453f97d36f16ee3e627a16b56da450eeaea441a
Rens Breur <rens.breur@live.com>
2022-07-05 12:11:58 +0200

    create Swift package for Axt

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "Axt",
+    products: [
+        .library(
+            name: "Axt",
+            targets: ["Axt"]),
+    ],
+    targets: [
+        .target(
+            name: "Axt",
+            dependencies: []),
+    ]
+)
---
+++ Sources/Axt/Axt.swift
@@ -0,0 +1,6 @@
+public struct Axt {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
