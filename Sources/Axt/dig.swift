ab682079435f4482cdc4cbc368d5caf3ed8fb0d5
Rens Breur <rens.breur@live.com>
2022-07-07 13:36:05 +0200

    fix indentation

--- Sources/Axt/dig.swift
+++ Sources/Axt/dig.swift
@@ -2,18 +2,18 @@ import Foundation
 
 #if TESTABLE
 
-    /// Recursively search for a property of a specific type in another object.
-    /// Works only for value types.
-    func dig<T>(for _: T.Type, in object: Any) -> T? {
-        if let result = object as? T { return result }
+/// Recursively search for a property of a specific type in another object.
+/// Works only for value types.
+func dig<T>(for _: T.Type, in object: Any) -> T? {
+    if let result = object as? T { return result }
 
-        for child in Mirror(reflecting: object).children {
-            if let result = dig(for: T.self, in: child.value) {
-                return result
-            }
+    for child in Mirror(reflecting: object).children {
+        if let result = dig(for: T.self, in: child.value) {
+            return result
         }
-
-        return nil
     }
+
+    return nil
+}
 
 #endif
