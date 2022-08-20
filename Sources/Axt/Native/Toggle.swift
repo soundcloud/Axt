0d77ba5e6d3be8e30db9223587b566d46472d0cc
Rens Breur <rens.breur@live.com>
2022-08-20 15:23:36 +0200

    Fix toggles not working on iOS 16

--- Sources/Axt/Native/Toggle.swift
+++ Sources/Axt/Native/Toggle.swift
@@ -4,16 +4,46 @@ public struct ToggleModifier<Content: View>: Modifier {
     #if TESTABLE
     public func make(_ content: Content) -> some View {
         let label = dig(for: String.self, in: content) ?? ""
-        let _isOn = dig(for: Binding<Bool>.self, in: content)
-        // By calling the wrappedValue of the binding, we also make sure
-        // SwiftUI does not optimize state updating away for this view, and
-        // so that it is ready when used in the `action` block as well.
-        let isOn = _isOn?.wrappedValue
-        return content.testData(label: label, value: isOn, action: { withAnimation { _isOn?.wrappedValue.toggle() } })
+        let isOn: Bool?
+        let action: () -> Void
+        if #available(iOS 16, *) {
+            // Starting with iOS 16, the state of a toggle is no longer a Bool,
+            // but an internal enum that can be in an on, off or mixed state.
+            let anyToggleStateBinding = digForProperty(named: "_toggleState", in: content)
+            let _toggleState = withUnsafePointer(to: anyToggleStateBinding) {
+                $0.withMemoryRebound(to: Binding<ToggleState>.self, capacity: 1) {
+                    $0.pointee
+                }
+            }
+            switch _toggleState.wrappedValue {
+            case .on: isOn = true
+            case .off: isOn = false
+            case .mixed: isOn = nil
+            }
+            action = {
+                if _toggleState.wrappedValue == .off {
+                    _toggleState.wrappedValue = .on
+                } else if _toggleState.wrappedValue == .on {
+                    _toggleState.wrappedValue = .off
+                }
+            }
+        } else {
+            let _isOn = dig(for: Binding<Bool>.self, in: content)
+            isOn = _isOn?.wrappedValue
+            action = { _isOn?.wrappedValue.toggle() }
+
+        }
+        return content.testData(label: label, value: isOn, action: { withAnimation { action() } })
     }
     #endif
 }
 
 public extension NativeView {
     static var toggle: NativeView<Content, ToggleModifier<Content>> { .init(base: .init()) }
+}
+
+private enum ToggleState {
+    case on
+    case off
+    case mixed
 }
--- Sources/Axt/dig.swift
+++ Sources/Axt/dig.swift
@@ -3,12 +3,23 @@ import Foundation
 #if TESTABLE
 
 /// Recursively search for a property of a specific type in another object.
-/// Works only for value types.
 func dig<T>(for _: T.Type, in object: Any) -> T? {
     if let result = object as? T { return result }
 
     for child in Mirror(reflecting: object).children {
         if let result = dig(for: T.self, in: child.value) {
+            return result
+        }
+    }
+
+    return nil
+}
+
+/// Recursively search for a property with a specific name in another object.
+func digForProperty(named name: String, in object: Any) -> Any? {
+    for child in Mirror(reflecting: object).children {
+        if child.label == name { return child.value }
+        if let result = digForProperty(named: name, in: child.value) {
             return result
         }
     }
